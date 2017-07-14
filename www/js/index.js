f242084076170fbdd4626f4a231e5e1a14828781
Paolo Leopardi <paolo@paololeopardi.com>
2017-07-14 18:52:46 +0200

    bug fix

--- www/js/index.js
+++ www/js/index.js
@@ -53,24 +53,3 @@ var app = {
 };
 
 app.initialize();
-
-// Add to index.js or the first page that loads with your app.
-// For Intel XDK and please add this to your app.js.
-
-document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
-
-  window.plugins.OneSignal
-    .startInit("YOUR_APPID")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-
-  // Call syncHashedEmail anywhere in your app if you have the user's email.
-  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
-}, false);
